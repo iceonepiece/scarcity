a8087add6394ad9d61a3b85dda8aa9f44a6a980a
iceonepiece <iceonepiece@gmail.com>
2022-12-23 19:44:54 +0700

    add memory leak detection code

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -1,10 +1,18 @@
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
 #include "Core/Game.h"
 
 int main()
 {
-  Game game;
-  game.Init();
-  game.Run();
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	
+	Game game;
+	game.Init();
+	game.Run();
 
-  return 0;
+	//_CrtDumpMemoryLeaks();
+
+	return 0;
 }
