5278d04cba2510a56d777d4e6d02ea00e6cb180b
iceonepiece <iceonepiece@gmail.com>
2023-08-10 08:11:16 +0700

    [refactor] <Editor> Generate premake file

--- Editor/Code/EditorApplication.cpp
+++ Editor/Code/EditorApplication.cpp
@@ -47,12 +47,6 @@ bool EditorApplication::NewProject(const std::string& name, std::filesystem::pat
 
         ScriptManager::CreateVisualStudioFiles(directory, name);
 
-        std::string cdCommand = "cd /d " + directory.string();
-        std::string premakeCommand = "premake5 vs2022";
-
-        std::system((cdCommand + " && " + premakeCommand).c_str());
-        FileUtils::RemoveFile(directory / "premake5.lua");
-
         std::unique_ptr<Scene> defaultScene = Scene::CreateDefaultScene(directory);
         defaultScene->SetApplication(this);
         defaultScene->Initialize();
--- Editor/Code/ScriptManager.h
+++ Editor/Code/ScriptManager.h
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <filesystem>
 
-const char* premakeContent = R"(
+const std::string PREMAKE_FILE_CONTENT = R"(
     architecture "x86_64"
 
     configurations { "Debug", "Release" }
@@ -25,21 +26,29 @@ project "Native-Script"
         optimize "On"
 )";
 
+const std::string PREMAKE_FILE_NAME = "premake5.lua";
+const std::string PREMAKE_COMMAND = "premake5 vs2022";
+
 class ScriptManager
 {
 public:
     static void CreateVisualStudioFiles(std::filesystem::path location , std::string name)
 	{
-        std::ofstream premakeFile(location / "premake5.lua");
+        std::ofstream premakeFile(location / PREMAKE_FILE_NAME);
 
         if (premakeFile.is_open())
         {
             premakeFile << "workspace \"" + name + "\"\n";
-            premakeFile << premakeContent;
+            premakeFile << PREMAKE_FILE_CONTENT;
+
+            std::string cdCommand = "cd /d " + location.string();
+
+            std::system((cdCommand + " && " + PREMAKE_COMMAND).c_str());
+            FileUtils::RemoveFile(location / PREMAKE_FILE_NAME);
         }
         else
         {
-            std::cout << "Error opening file: " << location / "premake5.lue" << std::endl;
+            std::cout << "Error opening file: " << location / PREMAKE_FILE_NAME << std::endl;
         }
 
         premakeFile.close();
