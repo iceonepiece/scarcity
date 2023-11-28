ce3de4eb8fb0ceb7c84cd0ebd52c9e6f3ba6c395
iceonepiece <iceonepiece@gmail.com>
2023-11-29 05:21:20 +0700

    <Editor> add animation clip preview in inspector

--- Editor/Code/EditorGUI/Inspectors/ImGui_AssetInspector.cpp
+++ Editor/Code/EditorGUI/Inspectors/ImGui_AssetInspector.cpp
@@ -62,7 +62,106 @@ void ImGui_AssetInspector::RenderAnimationClip(AnimationClip& animClip)
 {
     std::string name = animClip.GetPath().stem().string() + " (Animation Clip)";
 
+    static float frameTime = 0.1f;
+    static float timer = 0.0f;
+    static int currentFrame = 1;
+    static Texture* currentTexture = nullptr;
+    static bool isPlaying = false;
+
     ImGui::Text(name.c_str());
+
+    ImGui::BeginChild("Preview", ImVec2(0, 0));
+
+    ImGui::PushStyleVar(ImGuiStyleVar_SelectableTextAlign, ImVec2{ 0.5f, 0.5f });
+
+    currentTexture = animClip.GetImage()->GetTexture();
+    auto& spriteIndices = animClip.GetSpriteIndices();
+    auto& sprites = animClip.GetImage()->GetSprites();
+
+    ImGui::Text("Preview");
+    ImGui::Text("Total Frames: %d", spriteIndices.size());
+
+    ImGui::Text("Frame Time (Seconds): ");
+    ImGui::SameLine();
+    ImGui::InputFloat("##frameTime", &frameTime, 0);
+
+    if (spriteIndices.size() > 0)
+    {
+        auto it = spriteIndices.begin();
+
+        for (int i = 0; i < spriteIndices.size(); i++)
+        {
+            if (i == currentFrame - 1)
+                break;
+            it++;
+        }
+
+        if (it == spriteIndices.end())
+        {
+            currentFrame = 1;
+            it = spriteIndices.begin();
+        }
+
+        auto& sprite = sprites[*it];
+
+        if (currentTexture != nullptr)
+        {
+            const int SPRITE_WIDTH = 256;
+            const int HALF_SPRITE_WIDTH = SPRITE_WIDTH / 2;
+
+            int renderWidth = SPRITE_WIDTH;
+
+            if (sprite.GetWidth() < HALF_SPRITE_WIDTH)
+                renderWidth = HALF_SPRITE_WIDTH;
+            else if (sprite.GetWidth() > SPRITE_WIDTH)
+                renderWidth = SPRITE_WIDTH;
+
+            ImVec2 renderSize{ (float)renderWidth, renderWidth * (sprite.GetRatio().y / sprite.GetRatio().x) };
+            ImGui::Image((ImTextureID)currentTexture->GetRendererID(), renderSize, { sprite.GetLeft(), sprite.GetTop() }, { sprite.GetRight(), sprite.GetBottom() });
+        }
+    }
+
+    if (isPlaying)
+    {
+        timer += ImGui::GetIO().DeltaTime;
+
+        if (timer >= frameTime)
+        {
+            timer = 0.0f;
+            currentFrame++;
+
+            if (currentFrame > spriteIndices.size())
+                currentFrame = 1;
+        }
+
+        if (ImGui::Button(ICON_FA_PAUSE))
+            isPlaying = false;
+    }
+    else
+    {
+        if (ImGui::Button(ICON_FA_PLAY))
+        {
+            isPlaying = true;
+            timer = 0.0f;
+        }
+    }
+
+
+    ImGui::SameLine();
+
+    if (ImGui::Button(ICON_FA_STOP))
+    {
+        isPlaying = false;
+        currentFrame = 1;
+        timer = 0.0f;
+    }
+
+    ImGui::SameLine();
+    ImGui::SliderInt("Frame", &currentFrame, 1, spriteIndices.size());
+
+    ImGui::PopStyleVar();
+
+    ImGui::EndChild();
 }
 
 void ImGui_AssetInspector::RenderPrefabAsset(PrefabAsset& prefabAsset)
